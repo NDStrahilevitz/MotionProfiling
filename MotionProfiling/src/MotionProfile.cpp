834c32be7e26d7f72c7e85c68f5bc033a29f71fd
Nadav Strahilevitz <nd.strahilevitz@gmail.com>
2018-01-10 20:40:07 +0200

    Added solution for case where end setpoint time was ahead of "optimal" time

--- MotionProfiling/src/MotionProfile.cpp
+++ MotionProfiling/src/MotionProfile.cpp
@@ -13,16 +13,25 @@ void MotionProfile::Generate() {
 	float distCovered = 0;
 	float currTime = _start.GetTime();
 	float goalDist = _end.GetDist();
+	float goalTime = _end.GetTime();
 	float maxVel = _config._maxVel;
 	float maxAcc = _config._maxAcc;
+	float deAccel = maxAcc;
 	float dt = _config._dt; //sample time
 	float currVel = _start.GetVelocity();
 	float deltaVStart = maxVel - _start.GetVelocity();
 	float deltaVEnd = _end.GetVelocity() - maxVel;
 	float accelTime = deltaVStart / maxAcc;
-	float deaccelTime = deltaVEnd / maxAcc;
+	float accelDist = deltaVStart * accelTime / 2;
+	float deaccelTime = deltaVEnd / deAccel;
 	float deaccelDist = deltaVEnd * deaccelTime / 2;
-
+	float cruiseDist = goalDist - deaccelDist - accelDist;
+	float cruiseTime = cruiseDist / maxVel;
+	if (goalTime > cruiseTime + accelTime + deaccelTime) {
+		deAccel = fabs(deltaVEnd / (goalTime - (cruiseTime + accelTime)));
+		deaccelTime = deltaVEnd / deAccel;
+		deaccelDist = deltaVEnd * deaccelTime / 2;
+	}
 	while (currVel != maxVel) {
 		if (maxVel - currVel < maxAcc * dt) {
 			currVel = maxVel;
@@ -44,12 +53,14 @@ void MotionProfile::Generate() {
 	//now start deaccelerating
 	while (fabs(goalDist - distCovered) > _config._tolerance) {
 		currTime += dt;
-		currVel -= maxAcc * dt;
+		currVel -= deAccel * dt;
+		if (currVel <= 0)
+			break;
 		distCovered += currVel * dt;
 		Setpoint newSetpoint(currTime, distCovered, currVel);
 		_setpoints.push_front(newSetpoint);
 	}
-	_setpoints.push_front(_end);
+	//_setpoints.push_front(_end);
 	_setpoints.reverse(); //reverse linked list so it will be in order
 }
 
--- MotionProfiling/src/MotionProfiling.cpp
+++ MotionProfiling/src/MotionProfiling.cpp
@@ -9,8 +9,8 @@ namespace plt = matplotlibcpp;
 int main()
 {
 	Setpoint start(0, 0, 0);
-	Setpoint end(7.5, 50, 0);
-	MotionProfile profile(start, end, MotionProfileConfig(0.02,	5, 10, 0.1));
+	Setpoint end(0.5, 5, 0);
+	MotionProfile profile(start, end, MotionProfileConfig(0.02,	2, 10, 0.05));
 	profile.Generate();
 	std::forward_list<Setpoint> setpoints = profile.GetSetpoints();
 	std::vector<float> time;
